e595d7f33cfac2c34a8abdfd8511eb53ba1a6acd
Samson Danziger <samson.danziger@gmail.com>
2016-01-23 00:27:13 +0000

    add more python solutions

--- python/pe.py
+++ python/pe.py
@@ -1,5 +1,7 @@
 import euler
 from fractions import gcd
+import operator
+import functools
 
 # ----- Problem 1 ----- #
 def pe1():
@@ -13,7 +15,26 @@ def pe2():
 
 # ----- Problem 4 ----- #
 def pe4(): 
-    
+    max_palindrome = 0
+    for a in range(100, 1000):
+        for b in range(100, 1000):
+            val = a * b
+            if euler.isPalindrome(val) and val > max_palindrome:
+                max_palindrome = val
+    return max_palindrome
+
+
+# ----- Problem 5 ----- #
+def pe5():
+    # worked out minimum requirement of prime factors by hand. 
+    min_factors = [2, 2, 2, 2, 3, 3, 5, 7, 11, 13, 17, 19]
+    return functools.reduce(operator.mul, min_factors, 1)
+
+
+# ----- Problem 6 ----- #
+def pe6():
+    return abs(sum([i * i for i in range(1, 101)]) - (sum(range(1, 101))) ** 2)
+
 
 
 # ----- Problem 33 ----- #
